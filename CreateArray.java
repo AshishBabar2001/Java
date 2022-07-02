648bc3eea2ea8f5873a704af54fff251cf2e1ee1
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-07-02 10:19:34 +0530

    Add files via upload

---
+++ CreateArray.java
@@ -0,0 +1,15 @@
+public class CreateArray {
+    public static void main(String args[]){
+        int[] arr = new int[4];
+
+        arr[0] = 90;
+        arr[1] = 98;
+        arr[2] = 94;
+        arr[3] = 96;
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+    }
+}
